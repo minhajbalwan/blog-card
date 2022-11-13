c8a3cf39254a6e4eaac44026a50e4feabbf0ea9b
minhajbalwan <102814819+minhajbalwan@users.noreply.github.com>
2022-11-14 00:29:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// Btn Effect
+window.onload = () => {
+    document.querySelectorAll('.btn').forEach(btn => {
+        btn.innerHTML = `
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            ${btn.textContent}
+        `
+    });
+
+    // Card Hover Effect
+    document.querySelectorAll('.card').forEach(card => {
+        let content = card.querySelector('.content');
+        let footer = content.querySelector('.footer');
+        content.style.bottom = `-${footer.offsetHeight}px`;
+
+        card.addEventListener('mouseover', () => {
+            content.style.bottom = '0';
+        });
+
+        card.addEventListener('mouseleave', () => {
+            content.style.bottom = `-${footer.offsetHeight}px`;
+        });
+    })
+}
